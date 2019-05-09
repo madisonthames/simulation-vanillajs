b65d166efb81af8e4dfd6fca7cdb0809eb75b580
madison <madisonpaigethames@gmail.com>
2019-05-09 16:59:33 -0500

    final

---
+++ index.js
@@ -0,0 +1,14 @@
+// document.getElementById('add-button').addEventListener('click', function(){
+//     let inputTextName = document.getElementsById('expense-name-inputs')[0].value;
+//     let inputTextAmounts = document.getElementsById('expense-amounts-inputs')[0].value;
+//     let inputTextIcon = document.getElementsById('input')[0].value;
+// document.querySelector('expense-name-inputs').innerHTML += "<ol>" + inputTextName + "</ol>"
+// document.querySelector('expense-name-inputs').innerHTML += "<ol>" + inputTextAmounts + "</ol>"
+// document.querySelector('expense-name-inputs').innerHTML += "<ol>" + inputTextIcon + "</ol>"
+// })
+
+
+
+
+
+
